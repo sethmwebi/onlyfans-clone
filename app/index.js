91db270f52943f4045dc2ba4cd0bd0d660585b32
Seth Mwebi <sethmwebi27@gmail.com>
2023-08-30 09:50:17 +0300

    New post page

--- app/index.js
+++ app/index.js
@@ -1,11 +1,17 @@
-import { StyleSheet, View, FlatList} from "react-native";
+import { StyleSheet, View, FlatList } from "react-native";
+import { Link } from "expo-router";
 import UserCard from "../src/components/UserCard";
 import users from "../assets/data/users";
 
 export default function Page() {
   return (
     <View style={styles.container}>
-    <FlatList showsVerticalScrollIndicator={false} data={users} renderItem={({ item }) => <UserCard user={item}/>}/>
+      <Link href={"/newPost"}>New Post</Link>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        data={users}
+        renderItem={({ item }) => <UserCard user={item} />}
+      />
     </View>
   );
 }
